d2f88c267a9cd411c0785964fc1ad3fe95f4412d
Bruno Sampaio <brunoctsb@gmail.com>
2020-09-28 23:28:29 -0300

    Defined buttons for autolike

--- content.js
+++ content.js
@@ -1,7 +1,18 @@
 // Define variables 
 const profile = document.querySelector(".gmFkV")
-const interval = setInterval (() => {}, 2000)
-const inPhoto = document.querySelector("._2dDPU")
+const interval = setInterval (() => {
+    const inPhoto = document.querySelector("._2dDPU")
+    
+    if(inPhoto){
+        console.log("Photo here")
+        const nextPhoto = document.querySelector("._65Bje")
+        const like = document.querySelector("._8-yf5") 
+        clearInterval(interval)
+        
+    }
+}, 2000)
+
+console.log(profile)
 
 
 
