887065fae5bc0b26a2d7ab80d95443dccd994edf
Bruno Sampaio <brunoctsb@gmail.com>
2020-09-29 00:03:27 -0300

    Added AutoLike Funciton

--- content.js
+++ content.js
@@ -1,16 +1,27 @@
 // Define variables 
 const profile = document.querySelector(".gmFkV")
 const interval = setInterval (() => {
-    const inPhoto = document.querySelector("._2dDPU")
+const inPhoto = document.querySelector("._2dDPU")
     
     if(inPhoto){
         console.log("Photo here")
-        const nextPhoto = document.querySelector("._65Bje")
-        const like = document.querySelector("._8-yf5") 
         clearInterval(interval)
-        
+
+        const nextPhoto = document.querySelector("._65Bje")
+        const like = document.querySelector(`aria-label="Like"`)
+
+        console.log(nextPhoto)
+        console.log(like)
+
+        const buttonLike = document.createElement("button")
+        buttonLike.innerHTML = "AutoLike"
+        buttonLike.classList.add("AutoLikeButton")
+        inPhoto.appendChild(buttonLike)
+        buttonLike.addEventListener("click", AutoLike())     
     }
 }, 2000)
+
+
 
 console.log(profile)
 
@@ -18,6 +29,9 @@ console.log(profile)
 
 // Define AutoClick for like in photos
 
-
+function AutoLike(){
+    like.click()
+    nextPhoto.click()
+}
 
 // Create a list for followers
