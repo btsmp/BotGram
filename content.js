af5d0a38538b9dca24aef55cb9f45b292e2f9bfa
Bruno Sampaio <brunoctsb@gmail.com>
2020-09-28 23:09:03 -0300

    Create a project

---
+++ content.js
@@ -0,0 +1,12 @@
+// Define variables 
+const profile = document.querySelector(".gmFkV")
+const interval = setInterval (() => {}, 2000)
+const inPhoto = document.querySelector("._2dDPU")
+
+
+
+// Define AutoClick for like in photos
+
+
+
+// Create a list for followers
