2d1c56841987dce30783a4d76734e6779693fd31
Bruno Sampaio <brunoctsb@gmail.com>
2020-09-29 01:44:45 -0300

    Hot fix

--- content.js
+++ content.js
@@ -7,17 +7,19 @@ const inPhoto = document.querySelector("._2dDPU")
         console.log("Photo here")
         clearInterval(interval)
 
+        const buttonLike = document.createElement("button")
+        
+        buttonLike.innerHTML = "AutoLike"
+        buttonLike.classList.add("AutoLikeButton")
+        inPhoto.appendChild(buttonLike)        
+
         const nextPhoto = document.querySelector("._65Bje")
-        const like = document.querySelector(`aria-label="Like"`)
+        const like = document.querySelector(".QBdPU")
 
         console.log(nextPhoto)
         console.log(like)
-
-        const buttonLike = document.createElement("button")
-        buttonLike.innerHTML = "AutoLike"
-        buttonLike.classList.add("AutoLikeButton")
-        inPhoto.appendChild(buttonLike)
-        buttonLike.addEventListener("click", AutoLike())     
+        
+        // buttonLike.addEventListener("click", AutoLike())     
     }
 }, 2000)
 
@@ -29,9 +31,9 @@ console.log(profile)
 
 // Define AutoClick for like in photos
 
-function AutoLike(){
+/*function AutoLike(){
     like.click()
     nextPhoto.click()
 }
-
+*/
 // Create a list for followers
